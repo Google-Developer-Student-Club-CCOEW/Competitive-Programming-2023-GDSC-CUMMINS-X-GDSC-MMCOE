c66d713fe383567cf4176571b3f5c3c4be5ef412
rahul2001dewangan <rahul2001dewangan@gmail.com>
2023-10-01 11:58:25 +0530

    peak idex in mountain array

---
+++ peak.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"\nEnter the size of mountain array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"\nEnter the mountain array :\n";
+    int peak;
+    cin>>arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        cin>>arr[i];
+        if (arr[i]<arr[i-1])
+        {
+            peak = i-1;
+            break;
+        }
+    }
+    cout<<"\nThe peak index in the mountain array is : "<<peak<<endl;
+}
