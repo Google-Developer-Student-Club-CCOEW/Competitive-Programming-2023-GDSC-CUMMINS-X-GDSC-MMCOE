9a6b5185f400eb67668c4a7e673f5a1b7b3a1da4
Aryan Sahay <asryan2004@gmail.com>
2023-10-03 20:50:51 +0530

    Tower of Hanoi

---
+++ Tower_of_Hanoi.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+
+void solve(int a, int b, int c, int n) {
+	if (n == 0)
+		return;
+	solve(a, c, b, n-1);
+	cout<<a<<' '<<c<<endl;
+	solve(b, a, c, n-1);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    #ifdef LOCAL
+    freopen("input.txt", "r" , stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int n; cin>>n;
+    cout<< (1<<n) - 1<<endl;
+
+    solve(1, 2, 3, n);
+}
