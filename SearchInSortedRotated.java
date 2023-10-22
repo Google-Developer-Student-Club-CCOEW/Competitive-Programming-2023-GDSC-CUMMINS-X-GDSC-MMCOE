7707b317bd30fc29bbc64e05f260cc2597481487
Arshad Mahemood Patel <arshad.amp432@gmail.com>
2023-10-22 12:04:27 +0530

    Adding output

--- SearchInSortedRotated.java
+++ SearchInSortedRotated.java
@@ -25,3 +25,15 @@ class SearchInSortedRotated {
         return -1;
     }
 }
+
+// Example 1:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+
+// Example 3:
+// Input: nums = [1], target = 0
+// Output: -1
